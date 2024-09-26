5a3b1d7ccd40bbcc92b64b62e5fadb78e892dd6d
Ilias <ilias.dinia@outlook.com>
2024-09-26 12:46:34 +0300

    Implementing feedback list

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,7 +1,25 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
 export default function FeedbackList() {
   return (
     <ol className="feedback-list">
-      <li className="feedback"></li>
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>593</span>
+        </button>
+        <div>
+          <p>B</p>
+        </div>
+        <div>
+          <p>ByteGrad</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
+            qui facere obcaecati error ab nesciunt.
+          </p>
+        </div>
+        <p>4d</p>
+      </li>
     </ol>
   );
 }
