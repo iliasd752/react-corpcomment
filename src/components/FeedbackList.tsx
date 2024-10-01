8cd62790f409b9ea3ccd993f43962611440d4d52
Ilias <ilias.dinia@outlook.com>
2024-10-01 16:07:19 +0300

    Implemented error catcher

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
@@ -12,10 +14,17 @@ export default function FeedbackList() {
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     )
       .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
         return response.json();
       })
       .then((data) => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setErrorMessage("Something went wrong.");
         setIsLoading(false);
       });
   }, []);
@@ -23,6 +32,8 @@ export default function FeedbackList() {
   return (
     <ol className="feedback-list">
       {isLoading ? <Spinner /> : null}
+
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
