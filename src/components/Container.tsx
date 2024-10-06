53144c1bc762122b5961fd6bc0384e2a4ed5a6cf
Ilias <ilias.dinia@outlook.com>
2024-10-06 14:32:25 +0300

    Preparing code for refactoring

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,26 @@
+import { TFeedbackItem } from "../lib/types";
 import FeedbackList from "./FeedbackList";
 import Header from "./Header";
 
-export default function Container() {
+type ContainerProps = {
+  isLoading: boolean;
+  feedbackItems: TFeedbackItem[];
+  errorMessage: string;
+};
+
+export default function Container({
+  feedbackItems,
+  isLoading,
+  errorMessage,
+}: ContainerProps) {
   return (
     <main className="container">
       <Header />
-      <FeedbackList />
+      <FeedbackList
+        feedbackItems={feedbackItems}
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+      />
     </main>
   );
 }
