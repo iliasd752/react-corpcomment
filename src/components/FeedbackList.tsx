5055c8c8ce9bd7cf151492ea37b26d01d080d212
Ilias <ilias.dinia@outlook.com>
2024-10-06 14:12:44 +0300

    Code improvement

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -2,11 +2,28 @@ import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
 import ErrorMessage from "./ErrorMessage";
+import { TFeedbackItem } from "../lib/types";
 
 export default function FeedbackList() {
-  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
+  const handleAddToList = (text: string) => {
+    const companyName = text
+      .split(" ")
+      .find((word) => word.includes("#"))!
+      .substring(1);
+    const newItem: TFeedbackItem = {
+      text: text,
+      upvoteCount: 0,
+      daysAgo: 0,
+      companyName: companyName,
+      badgeLetter: companyName.substring(0, 1).toUpperCase(),
+    };
+
+    setFeedbackItems([...feedbackItems, newItem]);
+  };
 
   useEffect(() => {
     const fetchFeedbackItems = async () => {
@@ -34,9 +51,10 @@ export default function FeedbackList() {
 
   return (
     <ol className="feedback-list">
-      {isLoading ? <Spinner /> : null}
+      {isLoading && <Spinner />}
 
-      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
